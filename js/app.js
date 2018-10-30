4961a3b2a8cf410a84e0988b6cc668f0638b3e45
1stfaccia1 <chad.m.wittman75@gmail.com>
2018-10-30 01:21:22 -0500

    idk

--- js/app.js
+++ js/app.js
@@ -1,6 +1,9 @@
-/* particlesJS.load(@dom-id, @path-json, @callback (optional));
-*/
+particlesJS.load('particles', './particles.json';
+/*
+
 
 particlesJS.load('particles-js', 'particlesjs-config.json', function() {
 console.log('callback - particles.js config loaded');
 });
+
+*/
